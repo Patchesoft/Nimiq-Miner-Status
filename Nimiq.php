191cc26337886885359ac666b3efc3b1509e3f2c
Oli <patchesoft@gmail.com>
2018-04-03 13:37:18 -0400

    Updated for Release Candidate

--- Nimiq.php
+++ Nimiq.php
@@ -4,7 +4,11 @@ Class Nimiq {
 
 	var $result = "";
 	var $server = "";
+	var $timestamp = "";
 	var $data = array();
+	var $headers = array();
+	var $alias = "";
+	var $hide_active = 0;
 
 	/* Construct Nimiq Server
 	 *
@@ -14,9 +18,11 @@ Class Nimiq {
 	 * $url (STRING) - URL of the logfile to parse
 	 *
 	 */
-	public function __construct($url) 
+	public function __construct($url, $alias, $hide = 0) 
 	{
 		$this->server = $url;
+		$this->alias = $alias;
+		$this->hide_active = $hide;
 		$doc = $this->get_url($url);
 		$this->result = explode("\n",$doc);
 	}
@@ -36,29 +42,41 @@ Class Nimiq {
 		$hashrate = "";
 		$latest_block = "";
 		$timestamp = "";
+		$balance = "";
 		foreach($this->result as $line) {
 		    if(!empty($line)) {
 		        if($finder->find_phrase("Node: Wallet initialized for address", $line)) {
 		            $wallet = $finder->get_string_between($line, "Node: Wallet initialized for address", ".");
 		        }
-		        if($finder->find_phrase("Miner: Starting work on BlockHeader", $line)) {
-		            $hashrate = $finder->get_string_between($line, "hashrate=", "H/s");
+		        if($finder->find_phrase("Node: Hashrate: ", $line)) {
+		            $hashrate = $finder->get_string_between($line, "Node: Hashrate: ", "H/s");
 		        }
 		        if($finder->find_phrase("Node: Now at block", $line)) {
 		            $latest_block = $finder->get_string_between($line, "Node: Now at block: ", "");
 		        }
-		        if($finder->find_phrase("Miner: Starting work on BlockHeader", $line)) {
-		            $timestamp = $finder->get_string_between($line, "timestamp=", ",");
+		        if($finder->find_phrase("- Balance: ", $line)) {
+		            $balance = $finder->get_string_between($line, "- Balance: ", " NIM -");
 		        }
 		    }
+		}
+
+		// Get timestamp
+		if (isset($this->headers['last-modified'])) { 
+			$this->timestamp = $this->headers['last-modified'][0];
+			// Convert to timestamp
+			$datetime = DateTime::createFromFormat("D, d M Y H:i:s e", $this->timestamp);
+			$this->timestamp = $datetime->getTimestamp();
+		} else {
+			$this->timestamp = 0;
 		}
 
 		$this->data = array(
 			"wallet" => $wallet,
 			"hashrate" => $hashrate,
 			"latest_block" => $latest_block,
-			"timestamp" => $timestamp,
-			"server" => $this->server
+			"timestamp" => $this->timestamp,
+			"server" => $this->server,
+			"balance" => $balance
 		);
 
 		return $this;
@@ -73,13 +91,27 @@ Class Nimiq {
 	public function display() 
 	{
 		$timeout = "";
-		if(time() - $this->data['timestamp'] > 300) $timeout='<strong>NOT UPDATED</strong>';
+		if($this->hide_active == 0) {
+			if($this->data['timestamp'] == 0) {
+				$timeout = "<strong>Unable to get last-modified header</strong>";
+			} else {
+				if(time() - $this->data['timestamp'] > 300) {
+					$timeout='<strong>NOT UPDATED</strong>';
+				} else {
+					$timeout='<span class="label label-success small-text pull-right">Active</span>';
+				}
+			}
+		}
 		$html = '<div class="bubble">';
 		if(!empty($this->data['wallet'])) {
-		$html .= '<table>
-		    <tr><td width="100"><strong>Object</strong></td><td width="300"><strong>Data</strong></td></tr>
-		    <tr><td>Server</td><td>'. $this->data['server'] .'</td></tr>
-		    <tr><td>Wallet</td><td>'. $this->data['wallet'] .'</td></tr>
+		$html .= '<table class="">';
+		if(!empty($this->alias)) {
+			$html .= '<tr><td width="100">Server</td><td><strong>'. $this->alias .'</strong></td></tr>';
+		} else {
+			$html .= '<tr><td width="100">Server</td><td>'. $this->data['server'] .'</td></tr>';
+		}
+		$html .= '<tr><td>Wallet</td><td>'. $this->data['wallet'] .'</td></tr>
+		    <tr><td>Balance</td><td>'. number_format($this->data['balance'], 2) .' NIM</td></tr>
 		    <tr><td>HashRate</td><td>'.$this->data['hashrate'] .' H/s</td></tr>
 		    <tr><td>Latest Block</td><td>'.$this->data['latest_block'] .'</td></tr>
 		    <tr><td>Timestamp</td><td>'. date("jS F H:i:s", $this->data['timestamp']).' ' . $timeout . '</td></tr>
@@ -100,6 +132,7 @@ Class Nimiq {
 	private function get_url($request_url) {
 
 		$curl_handle = curl_init();
+		$headers = array();
 	    curl_setopt($curl_handle, CURLOPT_URL, $request_url);
 	    curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 0);
 	    //curl_setopt($curl_handle, CURLOPT_RANGE, '0-500');
@@ -108,10 +141,28 @@ Class Nimiq {
 	    curl_setopt($curl_handle, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
 	    curl_setopt($curl_handle, CURLOPT_FOLLOWLOCATION, 1);
 	    curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($curl_handle, CURLOPT_HEADERFUNCTION,
+		  function($curl, $header) use (&$headers)
+		  {
+		    $len = strlen($header);
+		    $header = explode(':', $header, 2);
+		    if (count($header) < 2) // ignore invalid headers
+		      return $len;
+
+		    $name = strtolower(trim($header[0]));
+		    if (!array_key_exists($name, $headers))
+		      $headers[$name] = [trim($header[1])];
+		    else
+		      $headers[$name][] = trim($header[1]);
+
+		    return $len;
+		  }
+		);
 	    $JsonResponse = curl_exec($curl_handle);
 	    $http_code = curl_getinfo($curl_handle);
+	    $this->headers = $headers;
 
-	  return($JsonResponse);
+	    return($JsonResponse);
 	}
 
 }
--- index.php
+++ index.php
@@ -2,19 +2,66 @@
 include("Finder.php");
 include("Nimiq.php");
 
+$servers = array(
+	array("http://coin6.patchesoft.com/logfile.log", "Pikachu1"),
+	array("http://coin6.patchesoft.com/logfile.log", "Bulbasaur1"),
+	array("http://coin6.patchesoft.com/logfile.log", "Squirtle1"),
+	array("http://coin6.patchesoft.com/logfile.log", "Charmander1"),
+);
 
-$server1 = new Nimiq("http://www.example.com/logfile.log");
-$server2 = new Nimiq("http://www.example2.com/logfile.log");
+$nimiqs = array();
+foreach($servers as $server) {
+	$nimiqs[] = new Nimiq($server[0], $server[1]);
+}
+
+
+/*$server1 = new Nimiq("http://coin6.patchesoft.com/logfile.log", "Pikachu1");
+$server2 = new Nimiq("http://coin6.patchesoft.com/logfile.log", "Bulbasaur1");
+$server3 = new Nimiq("http://coin6.patchesoft.com/logfile.log", "Squirtle1");*/
+//$server2 = new Nimiq("http://www.example2.com/logfile.log");
 
 
 ?>
-
-<h2>Nimiq Servers</h2>
-
-<?php echo $server1->get_data()->display(); ?>
-<?php echo $server2->get_data()->display(); ?>
-
-
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <style type="text/css">
-.bubble { display: inline-block; width: 400px; margin: 10px; }
+body {
+	color: #FFF;
+	background: #4776E6;  /* fallback for old browsers */
+	background: -webkit-linear-gradient(to bottom, #8E54E9, #4776E6);  /* Chrome 10-25, Safari 5.1-6 */
+	background: linear-gradient(to bottom, #8E54E9, #4776E6); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+	height: 100%;
+
+}
+.bubble { display: inline-block; width: 350px; margin: 10px; background: #FFF; border-radius: 4px; padding: 10px; color: #000 !important; border-left: 2px solid #e5811f;}
+.bubble table td { color: #000; }
+.small-text { font-size: 12px; }
+.header-margin { margin-bottom: 50px; }
 </style>
+</head>
+<body>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<h1 class="header-margin"><img src="nimiq.png"> Your NIMIQ Miners</h1>
+
+			<?php foreach($nimiqs as $nim) : ?>
+				<?php echo $nim->get_data()->display(); ?>
+			<?php endforeach; ?>
+
+			<hr>
+			<?php 
+			$total_hash = "";
+			$total_nims = 0;
+			foreach($nimiqs as $nim) {
+				$total_hash += $nim->data['hashrate'];
+				$total_nims += $nim->data['balance'];
+			}
+			echo "TOTAL HASHRATE: <strong>" .  number_format($total_hash) . " H/s</strong> - TOTAL BALANCE: <strong>" . number_format($total_nims, 2) . " NIM</strong> - TOTAL MINERS: <strong>" . count($nimiqs) . "</strong>";
+			?>
+		</div>
+	</div>
+</div>
+</body>
+</html>
